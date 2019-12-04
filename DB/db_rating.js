736e6830f05fc833f641dc0b59ac24bae465b99a
Icaruk <adri-rk@hotmail.com>
2019-12-04 16:32:05 +0100

    Query mongo relacional

--- DB/db_rating.js
+++ DB/db_rating.js
@@ -2,6 +2,55 @@
 const RatingModel = require("../models/Rating");
 
 
+// --------------------
+// TEST
+// --------------------
+
+const ObjectId = require("mongoose").Types.ObjectId;
+
+
+
+// RatingModel.aggregate([ 
+// 	{
+// 		$match: {raterId: ObjectId("5de6863f19d26244901c7ec2")}
+// 	},
+// 	{
+// 		$lookup: {
+// 			from: "users",
+// 			localField: "raterId",
+// 			foreignField: "_id",
+// 			as: "raterInfo"
+// 		}
+// 	},
+// 	{
+// 		$lookup: {
+// 			from: "users",
+// 			localField: "ratedId",
+// 			foreignField: "_id",
+// 			as: "ratedInfo"
+// 		}
+// 	},
+// 	{$unwind: "$ratedInfo"},
+// 	{$unwind: "$raterInfo"},
+// 	{
+// 		$project:
+// 		{
+// 			_id: 0,
+// 			value: 1,
+// 			comment: 1,
+// 			"ratedInfo.username": 1,
+// 			"raterInfo.username": 1,
+// 		}
+// 	}
+// ])
+// .then( (res) => {
+// 	console.log( res );
+// })
+// .catch( (err) => {
+// 	console.log("ERR: ", err );
+// });
+
+
 
 module.exports = {
 	
--- app.js
+++ app.js
@@ -26,58 +26,6 @@ app.use(require("./routes/rt_user"));
 
 
 
-// --------------------
-// TEST
-// --------------------
-
-const UserModel = require("./models/User");
-const RatingModel = require("./models/Rating");
-const ObjectId = require("mongoose").Types.ObjectId;
-
-
-
-RatingModel.aggregate([ 
-	{
-		$match: {raterId: ObjectId("5de6863f19d26244901c7ec2")}
-	},
-	{
-		$lookup: {
-			from: "users",
-			localField: "raterId",
-			foreignField: "_id",
-			as: "raterInfo"
-		}
-	},
-	{
-		$lookup: {
-			from: "users",
-			localField: "ratedId",
-			foreignField: "_id",
-			as: "ratedInfo"
-		}
-	},
-	{$unwind: "$ratedInfo"},
-	{$unwind: "$raterInfo"},
-	{
-		$project:
-		{
-			_id: 0,
-			value: 1,
-			comment: 1,
-			"ratedInfo.username": 1,
-			"raterInfo.username": 1,
-		}
-	}
-])
-.then( (res) => {
-	console.log( res );
-})
-.catch( (err) => {
-	console.log("ERR: ", err );
-});
-
-
-
 // Init srv
 app.listen(3000, ( () => {
 	console.log( "    ---> Server listening on port 3000" );
