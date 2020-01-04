574749be2858b5aa8992669f09c054558e4d045f
Dave86 <davidochando1986@gmail.com>
2020-01-04 19:19:37 +0100

    Endpoint busqueda con aggregate

--- DB/db_purchase.js
+++ DB/db_purchase.js
@@ -45,90 +45,161 @@ const getPurchase = async (req, res) => {
 
 	let query = req.query;
 
-	let id = query.id;
-	let buyerId = query.buyerId;
-	let sellerId = query.sellerId;
-	let destinationCountry = query.destinationCountry;
-	let destinationCity = query.destinationCity;
+	// let id = query.id;
+	// let buyerId = query.buyerId;
+	// let sellerId = query.sellerId;
+	// let destinationCountry = query.destinationCountry;
+	// let destinationCity = query.destinationCity;
+
+	// try {
+		
+	// 	let purchase;
+		
+		
+	// 	switch (true) {
+			
+	// 		case id:
+				
+	// 			purchase = await PurchaseModel.findById(
+	// 				id
+	// 			);
+				
+	// 			res.send(purchase);
+				
+	// 		break;
+			
+	// 		case buyerId:
+				
+	// 			purchase = await PurchaseModel.find({
+	// 				buyerId: buyerId
+	// 			});
+				
+	// 			res.send(purchase);
+				
+	// 		break;
+			
+	// 		case sellerId:
+				
+	// 			purchase = await PurchaseModel.find({
+	// 				sellerId: sellerId
+	// 			});
+				
+	// 			res.send(purchase);
+				
+	// 		break;
+			
+	// 		case destinationCountry:
+				
+	// 			purchase = await PurchaseModel.find({
+	// 				destinationCountry: destinationCountry
+	// 			});
+				
+	// 			res.send(purchase);
+				
+	// 		break;
+			
+	// 		case destinationCity:
+				
+	// 			purchase = await PurchaseModel.find({
+	// 				destinationCity: destinationCity
+	// 			});
+				
+	// 			res.send(purchase);
+				
+	// 		break;
+
+	// 		default:
+				
+	// 			purchase = await PurchaseModel.find({});
+				
+	// 			res.send(purchase);
+
+	// 		break;
+			
+	// 	};
+		
+		
+	// } catch (err) {
+		
+	// 	console.log( err );
+		
+	// };
+
+	// ----------------
+	// Stage LIMIT
+	// ----------------
 	
-	try {
-		
-		let purchase;
-		
-		
-		switch (true) {
-			
-			case id:
-				
-				purchase = await PurchaseModel.findById(
-					id
-				);
-				
-				res.send(purchase);
-				
-			break;
-			
-			case buyerId:
-				
-				purchase = await PurchaseModel.find({
-					buyerId: buyerId
-				});
-				
-				res.send(purchase);
-				
-			break;
-			
-			case sellerId:
-				
-				purchase = await PurchaseModel.find({
-					sellerId: sellerId
-				});
-				
-				res.send(purchase);
-				
-			break;
-			
-			case destinationCountry:
-				
-				purchase = await PurchaseModel.find({
-					destinationCountry: destinationCountry
-				});
-				
-				res.send(purchase);
-				
-			break;
-			
-			case destinationCity:
-				
-				purchase = await PurchaseModel.find({
-					destinationCity: destinationCity
-				});
-				
-				res.send(purchase);
-				
-			break;
+	// let limit = req.query.limit;	
+	
+	let stage_limit = 500;
 
-			default:
-				
-				purchase = await PurchaseModel.find({});
-				
-				res.send(purchase);
+	// ----------------
+	// Genero el array del AGGREGATE
+	// ----------------
+	
+	let arrAggregate = [];
 
-			break;
-			
-		};
+	// Stages obligatorias
+	arrAggregate = [
+		
+		...arrAggregate,
 		
 		
-	} catch (err) {
+		// { $sort: stage_sort },
+		// { $skip: stage_skip },
+		// { $match: stage_match },
+		{ $limit: stage_limit },
 		
+		{ $lookup:
+			{
+				from: "users",
+				localField: "sellerId",
+				foreignField: "_id",
+				as: "__sellerUsername"
+			}
+		},
+		{ $unwind: "$__sellerUsername"},
+		{ $addFields: {
+				_ownerUsername: "$__sellerUsername.username"
+			}
+		},
+		
+		{ $lookup:
+			{
+				from: "users",
+				localField: "buyerId",
+				foreignField: "_id",
+				as: "__buyerUsername"
+			}
+		},
+		{ $unwind: "$__buyerUsername"},
+		{ $addFields: {
+				_buyerUsername: "$__buyerUsername.username"
+			}
+		}
+		
+	];
+
+	
+	// ----------------
+	// Finalizo
+	// ----------------
+	
+	PurchaseModel.aggregate(arrAggregate).then( (purchase) => {
+		
+		if (purchase) {
+			res.send(purchase);
+		} else {
+			res.send({message: `Purchases not found.`});
+		}
+		
+	}).catch( (err) => {
 		console.log( err );
-		
-	};
+	});
 	
 	
 	
 };
-
 
 
 module.exports = {
